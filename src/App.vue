65ba963364d7271a5e81022efcdb7a1e2b4e305b
Mamoudou12 <bmamoudou463@gmail.com>
2024-08-06 14:11:33 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Vue 3 Todo APP</h1>
+  <form @submit="addNewTodo">
+    <label>New Todo</label>
+    <input name="newTodo">
+    <button>Add New Todo</button>
+  </form>
+</template>
+
+
+<script>
+  export default {
+
+  }
+</script>
+
+
+
+<style scoped>
+
+</style>
